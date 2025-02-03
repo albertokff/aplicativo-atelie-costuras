4a90f93444ad47c4bc0c6eecac002fc822b1ee5d
Diogo  Alberto <kirchoffalberto@gmail.com>
2025-02-03 01:36:23 -0300

    ✨feat: Corrigida ordenação e pontuação

--- aplicativo_mancada/lib/pages/game_page.dart
+++ aplicativo_mancada/lib/pages/game_page.dart
@@ -11,8 +11,8 @@ class GamePage extends StatefulWidget {
 }
 
 class _GamePageState extends State<GamePage> {
-  late Map<String, TextEditingController> pontuacao;
   late List<String> nomes;
+  Map<String, dynamic> pontuacao = {};
 
   @override
   void initState() {
@@ -21,16 +21,114 @@ class _GamePageState extends State<GamePage> {
     nomes = widget.nomeJogadores.split(',');
     pontuacao = {
       for (var nome in nomes) 
-        nome.trim(): TextEditingController(text: '0')
+        nome.trim(): {
+          'pontuacao': 0,
+          'posicao': 0
+        }
     };
   }
 
   @override
+
   void dispose() {
-    for (var controller in pontuacao.values) {
-      controller.dispose();
-    }
     super.dispose();
+  }
+
+  List<TableRow> _buildTableRows() {
+    var jogadoresOrdenados = pontuacao.entries.toList()
+      ..sort((a, b) => a.value['posicao'].compareTo(b.value['posicao']));
+
+    return jogadoresOrdenados.map((jogador) {
+      return TableRow(children: [
+        TableCell(
+            child: Center(
+                child: Text(
+                    pontuacao[jogador.key]['posicao'].toString()))),
+        TableCell(child: Center(child: Text(jogador.key))),
+        TableCell(
+            child: Center(
+                child: Text(
+                    pontuacao[jogador.key]['pontuacao'].toString()))),
+      ]);
+    }).toList();
+  }
+
+  void _mostrarModal() {
+    Map<String, TextEditingController> controladores = {};
+
+    pontuacao.forEach((jogador, pontos) {
+      controladores[jogador] = TextEditingController(text: '');
+    });
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // Modal com os TextFields
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          title: const Text(
+            'Atualize os pontos',
+            style: TextStyle(
+                color: Colors.green, fontWeight: FontWeight.bold),
+          ),
+          content: SizedBox(
+            height: 500, // Altura da modal
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: pontuacao.keys.map((jogador) {
+                return Padding( // Adicione Padding para espaçamento
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: TextField(
+                    controller: controladores[jogador],
+                    decoration: InputDecoration(
+                      labelText: jogador,
+                      border: OutlineInputBorder(), // Adicione borda ao TextField
+                    ),
+                    keyboardType: TextInputType.number,
+                  ),
+                );
+              }).toList(),
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Cancelar'),
+            ),
+            TextButton(
+              onPressed: () {
+                // Atualiza a pontuação de cada jogador
+                pontuacao.forEach((nome, dados) {
+                  int pontosRodada = int.tryParse(controladores[nome]!.text) ?? 0;
+
+                  pontuacao.update(nome, (dados) {
+                    return {
+                      'pontuacao': dados['pontuacao'] + pontosRodada,
+                      'posicao': dados['posicao']
+                    };
+                  });
+                });
+
+                setState(() {
+                  var jogadoresOrdenados = pontuacao.entries.toList()
+                    ..sort((a, b) => b.value['pontuacao'].compareTo(a.value['pontuacao']));
+
+                  for (int i = 0; i < jogadoresOrdenados.length; i++) {
+                    pontuacao[jogadoresOrdenados[i].key]['posicao'] = i + 1;
+                  }
+                }); // Atualiza a tabela
+                Navigator.of(context).pop();
+              },
+              child: const Text('Confirmar'),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -116,82 +214,62 @@ class _GamePageState extends State<GamePage> {
             Align(
               alignment: const Alignment(0, 0.2),
               child: Padding(
-                padding: const EdgeInsets.all(60.0),
-                child: Table(
-                  border: TableBorder.all(
-                    color: Colors.green,
-                    width: 2,
-                  ),
-                  columnWidths: const {
-                    0: FlexColumnWidth(1),
-                    1: FlexColumnWidth(2),
-                    2: FlexColumnWidth(1),
-                  },
-                  children: [
-                    const TableRow(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: Text(
-                            'Posição',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16,
-                                color: Colors.green),
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: Text(
-                            'Jogadores',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16,
-                                color: Colors.green),
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: Text(
-                            'Pontos',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16,
-                                color: Colors.green),
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                      ],
+                padding: const EdgeInsets.only(left: 60, right: 60),
+                child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,                
+                children: <Widget>[
+                  Table(
+                    border: TableBorder.all(
+                      color: Colors.green,
+                      width: 2.0,
+                      style: BorderStyle.solid
                     ),
-                    for (var nome in nomes)
-                      TableRow(
-                        children: [
-                          const Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: Text(
-                              '0',
-                              textAlign: TextAlign.center,
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                              nome.trim(),
-                              textAlign: TextAlign.center,
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                              pontuacao[nome.trim()]?.text ?? '0',
-                              textAlign: TextAlign.center,
-                            ),
-                          ),
-                        ],
-                      ),
-                  ],
+                    children: [
+                      const TableRow(children: [
+                        TableCell(
+                          child: 
+                            Center(
+                              child: 
+                                Text(
+                                  'Ranking',
+                                  style: 
+                                    TextStyle(
+                                      color: Colors.green
+                                    )
+                                )
+                            )
+                        ),
+                        TableCell(
+                          child: 
+                            Center(
+                              child: 
+                                Text(
+                                  'Jogador',
+                                  style: 
+                                    TextStyle(
+                                      color: Colors.green
+                                    )
+                                )
+                            )
+                        ),
+                        TableCell(
+                          child: 
+                            Center(
+                              child: 
+                                Text(
+                                  'Pontuação',
+                                  style: 
+                                    TextStyle(
+                                      color: Colors.green
+                                    )
+                                )
+                            )
+                        ),
+                      ]),
+                      ..._buildTableRows()
+                    ],
+                  ),
+                ],
                 ),
               ),
             ),
@@ -199,76 +277,7 @@ class _GamePageState extends State<GamePage> {
               alignment: const Alignment(0, 0.7),
               child: ElevatedButton(
                 onPressed: () {
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      // Modal com os TextFields
-                      return AlertDialog(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        title: const Text(
-                          'Atualize os pontos',
-                          style: TextStyle(
-                              color: Colors.green, fontWeight: FontWeight.bold),
-                        ),
-                        content: SizedBox(
-                          height: 500, // Altura da modal
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              for (var nome in nomes)
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: TextField(
-                                    controller: pontuacao[nome.trim()],
-                                    keyboardType: TextInputType.number,
-                                    decoration: InputDecoration(
-                                      labelText: nome.trim(),
-                                      border: const OutlineInputBorder(),
-                                    ),
-                                    onChanged: (String valor) {
-                                      debugPrint('nome considerado: $nome.trim()');
-                                      final int novoValor = int.tryParse(valor) ?? 0; 
-                                      pontuacao[nome.trim()]?.text = (int.tryParse(pontuacao[nome.trim()]!.text) !+ novoValor).toString(); 
-                                    },
-                                    
-                                  ),
-                                ),
-                            ],
-                          ),
-                        ),
-                        actions: [
-                          ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.green,
-                            ),
-                            onPressed: () {
-                              setState(() {
-                                for (var nome in nomes) {
-                                  pontuacao[nome.trim()]?.text = '0';
-                                }
-                              });
-                              Navigator.of(context).pop(); // Fecha a modal
-                            },
-                            child: const Text('Cancelar'),
-                          ),
-                          ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.green,
-                            ),
-                            onPressed: () {
-                              // debugPrint(pontuacao.toString());
-                              setState(() {});
-
-                              // Navigator.of(context).pop(); // Fecha a modal
-                            },
-                            child: const Text('Salvar'),
-                          ),
-                        ],
-                      );
-                    },
-                  );
+                  _mostrarModal();
                 },
                 style: ElevatedButton.styleFrom(
                   shape: RoundedRectangleBorder(
